a3a122e2d11afaa378b72140e2d96d8754b29970
John Kriewall <jwkriewall@gmail.com>
2021-01-14 11:31:30 -0500

    Exercises Day 4

--- module-1/03_Expressions/student-exercise/src/main/java/com/techelevator/Exercises.java
+++ module-1/03_Expressions/student-exercise/src/main/java/com/techelevator/Exercises.java
@@ -519,7 +519,7 @@ public class Exercises {
 	 */
 	public boolean nearTen(int num) {
 		if (num >= 0) {
-			if (num % 10 == 9 || num % 10 == 8 || num % 10 == 0 || num % 10 == 1 || num % 10 == 2) {
+			if (num % 10 == 8 || num % 10 == 9 || num % 10 == 0 || num % 10 == 1 || num % 10 == 2) {
 				return true;
 			}
 			return false;
---
+++ module-1/04_Loops_Arrays/student-lecture/src/main/java/com/techelevator/ArraysAndLoops.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+public class ArraysAndLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] instructors = new String[5];
+		
+		instructors[0] = "John";
+		instructors[1] = "Rachelle";
+		instructors[2] = "Steve";
+		instructors[3] = "Matt";
+		instructors[4] = "Kevin";
+	}
+
+}
