3fc85d8edd8b77f0c84d7ed4673b77f883130b9e
John Kriewall <jwkriewall@gmail.com>
2021-01-17 20:37:08 -0500

    Weekend Exercises Completed

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 	
 	//Write a command line program which prompts the user for a series of decimal integer values separated by spaces. 
@@ -7,7 +9,7 @@ public class DecimalToBinary {
 
 	//Please enter in a series of decimal values (separated by spaces): 460 8218 1 31313 987654321
 
-	//460 in binary is 111001100
+	//460 in binary is  111001100
 	//8218 in binary is 10000000011010
 	//1 in binary is 1
 	//31313 in binary is 111101001010001
@@ -15,6 +17,27 @@ public class DecimalToBinary {
 
 
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		String result = "";
+		
+		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
+		String numbersArray = input.nextLine();
+		String[] array = numbersArray.split(" ");
+		
+		int[] numbers = new int[array.length];	
+		for(int i = 0;i < array.length;i++)
+		{
+			numbers[i] = Integer.parseInt(array[i]);
+		}
+		
+		for (int i = 0; i < array.length; i++) {
+			
+		result = Integer.toBinaryString(numbers[i]);
+		
+		System.out.println(array[i] + " in binary is " + result);
+		}
 
 	}
 
