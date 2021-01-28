2f83030897bca6a446dc7def3fc66586d022ccfb
John <jwkriewall@gmail.com>
2021-01-27 20:48:03 -0500

    Week 3

--- module-1/12_Polymorphism/student-exercise/src/main/java/com/techelevator/BankCustomer.java
+++ module-1/12_Polymorphism/student-exercise/src/main/java/com/techelevator/BankCustomer.java
@@ -1,23 +1,24 @@
 package com.techelevator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BankCustomer {
 	
 	public BankCustomer() {}
 	
-	public BankCustomer(String name, String address, String phoneNumber, List <Accountable>accounts) {
-		this.name = name;
-		this.address = address;
-		this.phoneNumber = phoneNumber;
-		this.accounts = accounts;
-	}
+//	public BankCustomer(String name, String address, String phoneNumber, List <Accountable>accounts) {
+//		this.name = name;
+//		this.address = address;
+//		this.phoneNumber = phoneNumber;
+//		this.accounts = accounts;
+//	}
 	
 	private String name;
 	private String address;
 	private String phoneNumber;
-	private List <Accountable> accounts;
+	private List <Accountable> accounts = new ArrayList <Accountable>();
 	
 	
 	
@@ -43,34 +44,32 @@ public class BankCustomer {
 		return accounts;
 	}
 	
+	//private int account;
+	
+
 	
 	public boolean isVip() {
 		// for loop the accounts List to add each together.
 		// if sum is >= 25000, return true.
 		int total = 0;
-		List<Accountable> account = new ArrayList<Accountable>();
-		for (Accountable accounts: account) {
-			total += accounts.getBalance();
-			//total += account.get(0,1000);
-			System.out.println(total);
+		for (Accountable account : accounts) {
+			//total;
 		}
-		//if () {
-			
-	//	}
+		
+		
+		
+		
+//		for (Accountable account : accounts) {
+//			total += account;
+//		}
+		
 		return true;
 	}
 
 	
 	
 	public void addAccount(Accountable newAccount) {
-		
 		accounts.add(newAccount);
-		
-// I want to take accounts List and add an array to it.
-//		List<Accountable> allAccounts = new ArrayList<Accountable> ();
-//		for(Accountable accounts : allAccounts) {
-//			allAccounts.add(newAccount);
-//		}
 	}
 }	
 		
