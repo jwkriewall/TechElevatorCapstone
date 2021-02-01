00e11cd4135af99c8d77c53593e22101384a0f5b
John <jwkriewall@gmail.com>
2021-02-01 08:42:27 -0500

    Week 4

--- module-1/14_Unit_Testing/student-exercise/src/test/java/com/techelevator/WordCountTest.java
+++ module-1/14_Unit_Testing/student-exercise/src/test/java/com/techelevator/WordCountTest.java
@@ -1,5 +1,8 @@
 package com.techelevator;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 import org.junit.*;
@@ -17,7 +20,55 @@ public class WordCountTest {
 	public void key_with_multiple_words() {
 		String[] key1 = {"ba", "ba", "black", "sheep"};
 		Map<String, Integer> result = wordCount.getCount(key1);
-		//Assert.assertEquals({"ba" : 2, "black" : 1, "sheep" : 1}, result);
+		Map<String, Integer> test = new LinkedHashMap<String, Integer>();
+		test.put("ba", 2);
+		test.put("black", 1);
+		test.put("sheep", 1);
+		Assert.assertEquals(test, result);
+	}
+	
+	@Test
+	public void key_with_only_single_words() {
+		String[] key1 = {"ba", "black", "sheep"};
+		Map<String, Integer> result = wordCount.getCount(key1);
+		Map<String, Integer> test = new LinkedHashMap<String, Integer>();
+		test.put("ba", 1);
+		test.put("black", 1);
+		test.put("sheep", 1);
+		Assert.assertEquals(test, result);
+	}
+	
+	@Test
+	public void key_with_muliple_double_words() {
+		String[] key1 = {"ba", "ba", "black", "black", "sheep", "sheep"};
+		Map<String, Integer> result = wordCount.getCount(key1);
+		Map<String, Integer> test = new LinkedHashMap<String, Integer>();
+		test.put("ba", 2);
+		test.put("black", 2);
+		test.put("sheep", 2);
+		Assert.assertEquals(test, result);
+	}
+	
+	@Test
+	public void key_with_blank_string() {
+		String[] key1 = {"", "", "black", "sheep"};
+		Map<String, Integer> result = wordCount.getCount(key1);
+		Map<String, Integer> test = new LinkedHashMap<String, Integer>();
+		test.put("", 2);
+		test.put("black", 1);
+		test.put("sheep", 1);
+		Assert.assertEquals(test, result);
+	}
+	
+	@Test
+	public void key_with_null_String() {
+		String[] key1 = {null, "black", "sheep"};
+		Map<String, Integer> result = wordCount.getCount(key1);
+		Map<String, Integer> test = new LinkedHashMap<String, Integer>();
+		test.put(null, 1);
+		test.put("black", 1);
+		test.put("sheep", 1);
+		Assert.assertEquals(test, result);
 	}
 
 }
