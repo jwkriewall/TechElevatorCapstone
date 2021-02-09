ea30b4931b504178a631aeb688470acccab0b119
John <jwkriewall@gmail.com>
2021-02-09 13:14:08 -0500

    Day 2 Week 5

--- module-2/02_Aggregate_Functions_and_GROUP_BY/student-exercise/aggregate-functions-exercises.sql
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/student-exercise/aggregate-functions-exercises.sql
@@ -1,4 +1,4 @@
-﻿-- The following queries utilize the "world" database.
+-- The following queries utilize the "world" database.
 -- Write queries for the following problems. 
 -- Notes:
 --   GNP is expressed in units of one million US Dollars
@@ -13,45 +13,62 @@
 -- The results should be ordered alphabetically by state name and then by city 
 -- name. 
 -- (19 rows)
+SELECT (name || ', ' || district) AS name_and_state, population
+FROM city
+WHERE district IN('Pennsylvania', 'West Virginia', 'Kentucky', 'Indiana', 'Michigan')
+ORDER BY district, name;
 
 -- 2. The name, country code, and region of all countries in Africa.  The name and
 -- country code should be returned as a single column named country_and_code 
 -- and should contain values such as ‘Angola (AGO)’ 
 -- (58 rows)
 
+SELECT (name || ' (' || code || ')') AS country_and_code, region FROM country WHERE continent = 'Africa';
+
 -- 3. The per capita GNP (i.e. GNP multipled by 1000000 then divided by population) of all countries in the 
 -- world sorted from highest to lowest. Recall: GNP is express in units of one million US Dollars 
 -- (highest per capita GNP in world: 37459.26)
+SELECT name, gnp * 1000000 / population AS per_capita_GNP FROM country WHERE population !=0 AND gnp != 0 ORDER BY per_capita_GNP DESC;
 
 -- 4. The average life expectancy of countries in South America.
 -- (average life expectancy in South America: 70.9461)
+SELECT AVG(lifeexpectancy) FROM country WHERE continent = 'South America';
 
 -- 5. The sum of the population of all cities in California.
 -- (total population of all cities in California: 16716706)
+SELECT SUM(population) FROM city WHERE district = 'California';
 
 -- 6. The sum of the population of all cities in China.
 -- (total population of all cities in China: 175953614)
+SELECT SUM(population) FROM city WHERE countryCode = 'CHN';
 
 -- 7. The maximum population of all countries in the world.
 -- (largest country population in world: 1277558000)
+SELECT MAX(population) FROM country;
 
 -- 8. The maximum population of all cities in the world.
 -- (largest city population in world: 10500000)
+SELECT MAX(population) FROM city;
 
 -- 9. The maximum population of all cities in Australia.
 -- (largest city population in Australia: 3276207)
+SELECT MAX(population) FROM city WHERE countryCode = 'AUS';
 
 -- 10. The minimum population of all countries in the world.
 -- (smallest_country_population in world: 50)
+SELECT MIN(population) FROM country WHERE population > 0;
 
 -- 11. The average population of cities in the United States.
 -- (avgerage city population in USA: 286955.3795)
+SELECT AVG(population) FROM city WHERE countrycode = 'USA';
 
 -- 12. The average population of cities in China.
 -- (average city population in China: 484720.6997 approx.)
+SELECT AVG(population) FROM city WHERE countrycode = 'CHN';
 
 -- 13. The surface area of each continent ordered from highest to lowest.
 -- (largest continental surface area: 31881000, "Asia")
+SELECT SUM(surfacearea), continent FROM country GROUP BY continent ORDER BY SUM(surfacearea) DESC;
 
 -- 14. The highest population density (population divided by surface area) of all 
 -- countries in the world. 
--- module-2/02_Aggregate_Functions_and_GROUP_BY/student-lecture/lecture.sql
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/student-lecture/lecture.sql
@@ -1,56 +1,137 @@
 -- ORDERING RESULTS
 
 -- Populations of all countries in descending order
-
+SELECT name, population
+FROM country
+ORDER BY population desc;
 --Names of countries and continents in ascending order
+SELECT name, population
+FROM country
+ORDER BY surfacearea;
 
+SELECT name, (population / surfacearea) AS density FROM country ORDER BY density DESC;
+-- can order by aliases!
 -- LIMITING RESULTS
 -- The name and average life expectancy of the countries with the 10 highest life expectancies.
+SELECT name, lifeexpectancy FROM country WHERE lifeexpectancy IS NOT NULL ORDER BY lifeexpectancy DESC LIMIT 10;
 
+
+SELECT round(gnp / population, 4) FROM country WHERE gnp != 0;
 -- CONCATENATING OUTPUTS
 
 -- The name & state of all cities in California, Oregon, or Washington.
 -- "city, state", sorted by state then city
+SELECT name || ', ' || district || ': ' || population FROM city WHERE district IN ( 'California', 'Oregon' ,'Washington') 
+ORDER BY district, name;
 
 -- AGGREGATE FUNCTIONS
 -- Average Life Expectancy in the World
+SELECT AVG(lifeexpectancy) FROM country;
 
 -- Total population in Ohio
+SELECT SUM(population) FROM city WHERE district = 'Ohio';
 
 -- The surface area of the smallest country in the world
+SELECT MIN(surfacearea) FROM country;
 
 -- The 10 largest countries in the world
+SELECT name FROM country ORDER BY population DESC LIMIT 10;
 
 -- The number of countries who declared independence in 1991
+SELECT COUNT( * ) FROM country WHERE indepyear = 1991;
 
 -- GROUP BY
 -- Count the number of countries where each language is spoken, ordered from most countries to least
-
+SELECT language, COUNT(*) as number_of_countries
+FROM countrylanguage
+GROUP BY language
+ORDER BY number_of_countries DESC;
 -- Average life expectancy of each continent ordered from highest to lowest
+SELECT continent, AVG(lifeexpectancy) AS avg_life_expectancy
+FROM country
+WHERE lifeexpectancy IS NOT NULL
+GROUP BY continent
+ORDER BY avg_life_expectancy DESC;
 
 -- Exclude Antarctica from consideration for average life expectancy
 
 -- Sum of the population of cities in each state in the USA ordered by state name
+SELECT district, SUM(population) FROM city WHERE countrycode = 'USA' GROUP BY district ORDER BY district;
 
 -- The average population of cities in each state in the USA ordered by state name
+SELECT district, SUM(population), round(AVG(population), 4), MIN(population), MAX(population), COUNT(*) 
+FROM city WHERE countrycode = 'USA' GROUP BY district ORDER BY district;
+
+
+--
+SELECT continent, region, SUM(surfacearea) FROM country
+GROUP BY continent, region
+ORDER BY continent, region;
 
 -- SUBQUERIES
+-- Find the names of cities in Europe with a gnp > 1000000
+SELECT code FROM country WHERE continent = 'Europe' AND gnp > 1000000;
+
+SELECT *
+FROM city
+WHERE countrycode IN (SELECT code FROM country WHERE continent = 'Europe' AND gnp > 1000000);
 -- Find the names of cities under a given government leader
+SELECT code FROM country WHERE headofstate = 'Elisabeth II';
+
+SELECT * FROM city
+WHERE countrycode IN(SELECT code FROM country WHERE headofstate = 'Elisabeth II') ORDER BY population DESC, countrycode;
 
 -- Find the names of cities whose country they belong to has not declared independence yet
+SELECT * FROM city WHERE countrycode IN(SELECT code FROM country WHERE indepyear IS NULL);
+
+-- Find the sum of the population of each district in the country with the largest surface area
+SELECT district, SUM(population) FROM city
+WHERE countrycode =(SELECT code FROM country ORDER BY surfacearea DESC LIMIT 1)
+GROUP BY district
+ORDER BY district;
+
 
 -- Additional samples
 -- You may alias column and table names to be more descriptive
+SELECT name  AS city_name
+FROM city AS cities;
 
 -- Alias can also be used to create shorthand references, such as "c" for city and "co" for country.
+SELECT city.name, country.name
+FROM city, country;
+
+SELECT c.name, co.name
+FROM city AS c, country AS co;
+
+SELECT c.name, co.name
+FROM city c, country co;
 
 -- Ordering allows columns to be displayed in ascending order, or descending order (Look at Arlington)
+SELECT name, population
+FROM city
+WHERE countrycode = 'USA'
+ORDER BY name ASC, population DESC;
 
 -- Limiting results allows rows to be returned in 'limited' clusters,where LIMIT says how many, and OFFSET (optional) specifies the number of rows to skip
 
+SELECT name, population
+FROM city
+LIMIT 10 OFFSET 20;
+
+
 -- Most database platforms provide string functions that can be useful for working with string data. In addition to string functions, string concatenation is also usually supported, which allows us to build complete sentences if necessary.
+SELECT (name || ' is in the state of ' || district) AS city_and_state
+FROM city
+WHERE countrycode = 'USA';
+
 
 -- Aggregate functions provide the ability to COUNT, SUM, and AVG, as well as determine MIN and MAX. Only returns a single row of value(s) unless used with GROUP BY.
+SELECT COUNT(name) AS city_count
+FROM city;
+
+SELECT COUNT(population) FROM city;
+SELECT COUNT(countrycode)FROM city;
+
 
 -- Counts the number of rows in the city table
 
@@ -58,7 +139,13 @@
 
 -- Gets the SUM of the population field in the city table, as well as
 -- the AVG population, and a COUNT of the total number of rows.
+SELECT COUNT(*), SUM(population), AVG(population)
+FROM city;
 
 -- Gets the MIN population and the MAX population from the city table.
+SELECT MIN(population), MAX(population) FROM city;
 
--- Using a GROUP BY with aggregate functions allows us to summarize information for a specific column. For instance, we are able to determine the MIN and MAX population for each countrycode in the city table.
+-- Using a GROUP BY with aggregate functions allows us to summarize information for a specific column. 
+-- For instance, we are able to determine the MIN and MAX population for each countrycode in the city table.
+SELECT countrycode, MIN(population), MAX(population) FROM city GROUP BY countrycode;
+
