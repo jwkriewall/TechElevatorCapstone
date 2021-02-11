770eb04194bb28213fc0952ee507fe2e595a26e1
John <jwkriewall@gmail.com>
2021-02-11 15:05:40 -0500

    Week 5

--- module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
@@ -2,19 +2,40 @@
 -- The following changes are applied to the "dvdstore" database.**
 
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
+SELECT * FROM actor
+ORDER BY last_name;
+INSERT INTO actor (first_name, last_name) VALUES ('HAMPTON', 'AVENUE');
+INSERT INTO actor (first_name, last_name) VALUES ('LISA', 'BYWAY');
 
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in
 -- ancient Greece", to the film table. The movie was released in 2008 in English.
--- Since its an epic, the run length is 3hrs and 18mins. There are no special
+-- Since its an epic, the run length is 3hrs and 18mins (198). There are no special
 -- features, the film speaks for itself, and doesn't need any gimmicks.
+
+SELECT * FROM film;
+INSERT INTO film (title, description, release_year, language_id, length) 
+VALUES ('EUCLIDEAN PI', 'The epic story of Euclid as a pizza delivery boy in ancient Greece', 2008, 1, 198);
 
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
+SELECT * FROM film
+JOIN film_actor ON film_actor.film_id = film.film_id
+JOIN actor ON film_actor.actor_id= actor.actor_id
+WHERE film.title = 'EUCLIDEAN PI';
+UPDATE film SET actor.first_name = 'HAMPTON', actor_last_name = 'AVENUE';
+
 
 -- 4. Add Mathmagical to the category table.
+SELECT * FROM category;
+INSERT INTO category (name) VALUES ('Mathmagical');
 
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
+SELECT * FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id;
+UPDATE film SET category.name = 'Mathmagical'
+WHERE film.title = 'EUCLIDEAN PI';
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
 -- accordingly.
