553c7b4df4276ce525fc03f2b904369ff7efedcf
John <jwkriewall@gmail.com>
2021-02-10 20:58:21 -0500

    Week 5 Day 3

--- module-2/03_Joins/student-exercise/joins-exercises.sql
+++ module-2/03_Joins/student-exercise/joins-exercises.sql
@@ -42,34 +42,83 @@ WHERE category.name = 'Comedy';
 
 -- 6. All of the ‘Children’ films that are rated ‘G’
 -- (10 rows)
-SELECT *
+SELECT film.title
 FROM film
-JOIN --------
-WHERE category.name = 'Children';
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE category.name = 'Children' AND rating = 'G';
 
 -- 7. All of the ‘Family’ films that are rated ‘G’ and are less than 2 hours in length
 -- (3 rows)
+SELECT film.title
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE category.name = 'Family' AND rating = 'G' AND film.length < 120;
 
 -- 8. All of the films featuring actor Matthew Leigh that are rated ‘G’
 -- (9 rows)
+SELECT *
+FROM film
+JOIN film_actor ON film_actor.film_id = film.film_id
+JOIN actor ON actor.actor_id = film_actor.actor_id
+JOIN film_category ON film_category.film_id = film.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE actor.first_name = 'MATTHEW' AND actor.last_name = 'LEIGH' AND film.rating = 'G';
 
 -- 9. All of the ‘Sci-Fi’ films released in 2006
 -- (61 rows)
+SELECT film.title
+FROM film
+JOIN film_category ON film_category.film_id = film.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE category.name = 'Sci-Fi' AND film.release_year = '2006';
 
 -- 10. All of the ‘Action’ films starring Nick Stallone
 -- (2 rows)
+SELECT *
+FROM actor
+JOIN film_actor ON film_actor.film_id = actor.actor_id
+JOIN film ON film.film_id = film_actor.film_id
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE category.name = 'Action' AND actor.first_name = 'NICK' AND actor.last_name = 'STALLONE';
 
 -- 11. The address of all stores, including street address, city, district, and country
 -- (2 rows)
+SELECT address.address, city.city, country.country, address.district
+FROM store
+JOIN address ON address.address_id = store.address_id
+JOIN city ON city.city_id = address.city_id
+JOIN country ON city.country_id = country.country_id;
 
 -- 12. A list of all stores by ID, the store’s street address, and the name of the store’s manager
 -- (2 rows)
+SELECT staff.store_id, address.address, staff.first_name, staff.last_name
+FROM store
+JOIN staff ON store.store_id = staff.store_id
+JOIN address ON address.address_id = store.address_id
+GROUP BY staff.store_id, address.address, staff.first_name, staff.last_name;
+
 
 -- 13. The first and last name of the top ten customers ranked by number of rentals 
 -- (#1 should be “ELEANOR HUNT” with 46 rentals, #10 should have 39 rentals)
+SELECT customer.first_name, customer.last_name, COUNT(customer.customer_id)
+FROM customer
+JOIN payment ON customer.customer_id = payment.customer_id
+GROUP BY customer.first_name, customer.last_name
+ORDER BY COUNT(customer.customer_id) DESC
+LIMIT 10;
 
 -- 14. The first and last name of the top ten customers ranked by dollars spent 
 -- (#1 should be “KARL SEAL” with 221.55 spent, #10 should be “ANA BRADLEY” with 174.66 spent)
+
+SELECT customer.first_name, customer.last_name, SUM(payment.amount) AS total_spent
+FROM customer
+JOIN payment ON customer.customer_id = payment.customer_id
+GROUP BY customer.first_name, customer.last_name
+ORDER BY total_spent DESC
+LIMIT 10;
 
 -- 15. The store ID, street address, total number of rentals, total amount of sales (i.e. payments), and average sale of each store.
 -- (NOTE: Keep in mind that an employee may work at multiple stores.)
@@ -85,6 +134,13 @@ GROUP BY store.store_id, address.address;
 
 -- 16. The top ten film titles by number of rentals (LIMIT 10)
 -- (#1 should be “BUCKET BROTHERHOOD” with 34 rentals and #10 should have 31 rentals)
+SELECT film.title, COUNT(rental.customer_id) AS number_of_rentals
+FROM film
+JOIN inventory ON film.film_id = inventory.film_id
+JOIN rental ON inventory.inventory_id = rental.inventory_id
+GROUP BY film.title
+ORDER BY number_of_rentals DESC
+LIMIT 10;
 
 
 -- 17. The top five film categories by number of rentals 
