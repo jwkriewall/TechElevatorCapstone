b7f3dcb9fac793c3ce1844c915b963532f6179f1
Ben Baker <bjbaker1@gmail.com>
2021-04-05 12:46:07 -0400

    Merging

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -53,6 +53,14 @@ const router = new Router({
         requiresAuth: false
       }
     },
+    {
+      path: "/create",
+      name: "create",
+      component: Create,
+      meta: {
+        requiresAuth: true
+      }
+    }
   ]
 })
 
