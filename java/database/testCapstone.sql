3b72b1c6e00cb81e3397efa28693ddcbf1a00806
Student <student@techs-air.lan>
2021-04-06 13:04:43 -0400

    Update

--- java/database/testCapstone.sql
+++ java/database/testCapstone.sql
@@ -1,11 +1,18 @@
 START TRANSACTION;
+ROLLBACK;
+
 INSERT INTO organizer (user_id, organizer_first_name, organizer_last_name, organizer_phone, organizer_email)
         VALUES (?, 'Nick', 'Stewart', 7777777777, 'nickstewart@gmail.com');
+        
+UPDATE tournament
+SET tournament_name = ?, organizer_id = ?, max_participants = ?, is_team = ?, is_double = ?, start_date = ?, end_date = ?
+WHERE id = ?;        
+
         
 INSERT INTO users (user_id, username, password_hash, role) VALUES (?, ?, ?, ?);
 
 
-INSERT INTO tournament (tournament_name, organizer_id, max_participants, is_team, is_double) VALUES (?, ?, ?, ?, ?);
+INSERT INTO tournament (tournament_name, organizer_id, max_participants, is_team, is_double, start_date, end_date) VALUES (?, ?, ?, ?, ?, ?, ?);
         
 SELECT tournament_name, max_participants, is_team, is_double, organizer_first_name, organizer_last_name, organizer_phone, organizer_email FROM tournament
 JOIN organizer ON organizer.organizer_id = tournament.organizer_id;
@@ -25,6 +32,11 @@ SELECT organizer_id, users.user_id, organizer_first_name, organizer_last_name, o
 FROM organizer
 JOIN users ON organizer.user_id = users.user_id
 WHERE users.user_id = ?;
+
+SELECT * FROM tournament;
+
+DELETE FROM tournament
+WHERE id = ?;
 
 
 SELECT CASE users.user_id WHEN (users.user_id != null AND organizer_id != null)  THEN true
